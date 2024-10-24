204e51d4f62ebdfd9d5a5307d1f83f177a5bf478
yash sharma <yash@saal.ai>
2024-10-24 17:39:40 +0400

    add update and delete function

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from fastapi import Depends, FastAPI
 
 from core.deps import EngineDep
-from models.Todo import Todo
+from models.Todo import Todo, PatchTodoSchema
+from odmantic import ObjectId
 
 app = FastAPI()
 
@@ -16,3 +17,18 @@ async def read(engine: EngineDep):
     todos = await engine.find(Todo)
     return todos
 
+@app.put("/{id}")
+async def update(engine: EngineDep, item: PatchTodoSchema, id: ObjectId):
+    todo = await engine.find_one(Todo, Todo.id == id)
+    if todo:
+        update_data = item.model_dump(exclude_unset=True)
+        for key, value in update_data.items():
+            setattr(todo, key, value)
+        await engine.save(todo)
+        return todo
+    return {"error": "Todo not found"}
+
+@app.delete("/{id}")
+async def delete(engine: EngineDep, id: ObjectId):
+    todo = await engine.remove(Todo, Todo.id == id)
+    return todo
--- app/models/Todo.py
+++ app/models/Todo.py
@@ -1,9 +1,14 @@
 from odmantic import Model, Field
 from datetime import datetime, timezone
 from typing import Optional
-
+from pydantic import BaseModel
 class Todo(Model):
     title: str
     description: Optional[str] = None
     created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
     completed: bool = False
+
+class PatchTodoSchema(BaseModel):
+    title: Optional[str] = None
+    description: Optional[str] = None
+    completed: Optional[bool] = None
